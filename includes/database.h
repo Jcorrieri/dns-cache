9751f8986cba381132950dd67f569d23143c6310
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-09-10 18:35:57 -0400

    changes

---
+++ includes/database.h
@@ -0,0 +1,8 @@
+#ifndef JC_BP_DB
+#define JC_BP_DB
+
+#include <string_view>
+
+void init_database(const std::string_view db_name);
+
+#endif
--- src/database.cpp
+++ src/database.cpp
@@ -1,3 +1,16 @@
+#include <filesystem>
+#include "../external/sqlite-amalgamation-3530400/sqlite3.h" // TODO: Fix this bologne....
+
+void init_database(const std::string_view db_name) {
+    if (!std::filesystem::exists(db_name)) {
+        return;
+    }
+}
+
 int main() {
+    const std::string db_name{"temp.db"};
+
+    sqlite3 *db = nullptr;
+
     return 0;
 }
